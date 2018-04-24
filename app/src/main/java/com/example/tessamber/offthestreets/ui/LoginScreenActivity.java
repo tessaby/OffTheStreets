b607bf2f066e765e310dc40458178d93d21a24cf
Steven Yuh <stevenyuh96@gmail.com>
2018-04-24 15:01:33 -0400

    fb integration

--- app/src/main/java/com/example/tessamber/offthestreets/ui/LoginScreenActivity.java
+++ app/src/main/java/com/example/tessamber/offthestreets/ui/LoginScreenActivity.java
@@ -1,9 +1,11 @@
 package com.example.tessamber.offthestreets.ui;
 
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +13,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.tessamber.offthestreets.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -36,12 +45,19 @@ public class LoginScreenActivity extends AppCompatActivity {
     private EditText etPassword;
     // DECLARE text view message
     //private TextView tvMessage;
+    CallbackManager callbackManager;
+    LoginButton login_button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login_screen);
+        initializeControls();
+        loginWithFB();
+
+
 
         //INITIALIZE:
 
@@ -110,10 +126,40 @@ public class LoginScreenActivity extends AppCompatActivity {
         });
     }
 
+    private void initializeControls() {
+        callbackManager = CallbackManager.Factory.create();
+        login_button = (LoginButton)findViewById(R.id.login_button);
+
+    }
+
+    private void loginWithFB(){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                toastMessage("login successful");
+            }
+
+            @Override
+            public void onCancel() {
+                toastMessage("login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                toastMessage("login error. try again." + error.getMessage());
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
     /**
      * customizable toast
      * @param message message to be displayed
      */
+
     private void toastMessage(CharSequence message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
